d09c9eefcccfd47e8f3202fb3c94c322df77be46
Israel Inacio Junior <israelijr@gmail.com>
2026-07-11 12:10:34 -0300

    atualização de versão

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -16,8 +16,8 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
-  String _version = '1.0.32';
-  String _buildNumber = '39';
+  String _version = '1.0.33';
+  String _buildNumber = '40';
 
   // Contador para easter egg (7 toques na versão = abre PremiumDebugScreen)
   int _debugTapCount = 0;
